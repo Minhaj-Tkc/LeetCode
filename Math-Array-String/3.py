329598089b4f117c106b5d14410610cc933ea876
Minhaj Tkc <minhajtkc@gmail.com>
2024-10-19 17:29:51 +0530

    2469. Convert the Temperature

---
+++ Math-Array-String/3.py
@@ -0,0 +1,15 @@
+# 2469. Convert the Temperature
+
+from typing import List
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius + 273.15 , celsius * 1.80+32.00]
+    
+
+celsius = 36.50
+
+sol = Solution()
+result = sol.convertTemperature(celsius)
+
+print(result) # [309.65, 97.7]
