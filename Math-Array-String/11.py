bd5154d5beb5a29ab384787d0587e209c9822e4c
Minhaj Tkc <minhajtkc@gmail.com>
2024-10-27 17:12:38 +0530

    1979. Find Greatest Common Divisor of Array

---
+++ Math-Array-String/11.py
@@ -0,0 +1,28 @@
+# 1979. Find Greatest Common Divisor of Array
+
+
+from typing import List
+
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        mn = min(nums)
+        mx = max(nums)
+        for i in range(1, mn+1):
+            if mn % i == 0 and mx % i == 0:
+                gcd = i
+        return gcd
+    
+
+sol = Solution()
+nums = [3,3]
+output = sol.findGCD(nums)
+print(output)
+
+
+# import math
+# class Solution:
+#     def findGCD(self, nums: List[int]) -> int:
+#         mn=min(nums)
+#         mx=max(nums)
+#         gcd=math.gcd(mn, mx)
+#         return gcd
