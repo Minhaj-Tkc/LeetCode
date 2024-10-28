8120d0ab0c764496873285264ddd377a407aeb12
Minhaj Tkc <minhajtkc@gmail.com>
2024-10-28 14:10:10 +0530

    1108. Defanging an IP Address

---
+++ Math-Array-String/12.py
@@ -0,0 +1,17 @@
+# 1108. Defanging an IP Address
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return ''.join(['[.]' if char == "." else char for char in address])
+    
+    
+sol = Solution()
+address = "255.100.50.0"
+output = sol.defangIPaddr(address)
+print(output)
+
+
+# Using replace method
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace('.', '[.]')
