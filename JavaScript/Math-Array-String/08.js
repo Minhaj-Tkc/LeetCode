6698d1da3d2c44a9e956f9075f9baecbd03d4d00
Minhaj Tkc <minhajtkc@gmail.com>
2024-11-08 12:03:16 +0530

    1614. Maximum Nesting Depth of the Parentheses .js

---
+++ JavaScript/Math-Array-String/08.js
@@ -0,0 +1,27 @@
+// 1614. Maximum Nesting Depth of the Parentheses
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+
+    let maxDepth = 0;
+    let currentDepth = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            currentDepth++;
+            maxDepth = Math.max(maxDepth, currentDepth);
+        } else if (s[i] === ')') {
+            currentDepth--;
+        }
+    }
+    
+    return maxDepth;
+
+};
+
+
+console.log(maxDepth("()(())((()()))"));
