394bbb1944e71618cdedb377d4334d94c4e22d5d
Minhaj Tkc <minhajtkc@gmail.com>
2024-12-01 13:03:08 +0530

    Robot Return to Origin Updated

--- JavaScript/Math-Array-String/16.js
+++ JavaScript/Math-Array-String/16.js
@@ -5,9 +5,12 @@
  * @return {boolean}
  */
 var judgeCircle = function(moves) {
+
     let x = 0; 
     let y = 0;
+
     for(let i of moves){  
+
         if(i === "U"){
             x += 1;
         } else if(i === "D"){
@@ -17,10 +20,13 @@ var judgeCircle = function(moves) {
         } else if(i === "R"){
             y -= 1;
         }
+
     }
+
     console.log(x, y);
     
     return x === 0 && y === 0;
+    
 };
 
 console.log(judgeCircle("UD"));
