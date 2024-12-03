8bcf8972a5d58cb147236f0a38d1e6536600d38c
Minhaj Tkc <minhajtkc@gmail.com>
2024-12-03 22:49:36 +0530

    Truncate Sentence Problem Updated

--- Python/Math-Array-String/25.py
+++ Python/Math-Array-String/25.py
@@ -4,10 +4,8 @@ class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
         # Split the sentence into words
         words = s.split()
-        print(words)
         # Select the first k words and join them with spaces
         return " ".join(words[:k])
-
 
 
 sol = Solution()
