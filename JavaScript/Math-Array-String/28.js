b8464b851f09456345eec3fcd0f848094a34d62b
Minhaj Tkc <minhajtkc@gmail.com>
2024-12-12 21:36:56 +0530

    2894. Divisible and Non-divisible Sums Difference

---
+++ JavaScript/Math-Array-String/28.js
@@ -0,0 +1,26 @@
+// 2894. Divisible and Non-divisible Sums Difference
+
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let num1 = 0; // Sum of numbers not divisible by m
+    let num2 = 0; // Sum of numbers divisible by m
+    
+    for (let i = 1; i <= n; i++) {
+        if (i % m === 0) {
+            num2 += i;
+        } else {
+            num1 += i;
+        }
+    }
+    
+    return num1 - num2;
+};
+
+// Example usage:
+let n = 10;
+let m = 3;
+console.log(differenceOfSums(n, m)); // Output: 19
