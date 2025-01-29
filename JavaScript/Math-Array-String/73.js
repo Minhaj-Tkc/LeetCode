12041b21ced505d341a4a115634a0462f78d6130
Minhaj Tkc <minhajtkc@gmail.com>
2025-01-29 12:52:21 +0530

    242. Valid Anagram

---
+++ JavaScript/Math-Array-String/73.js
@@ -0,0 +1,27 @@
+// 242. Valid Anagram
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    
+    let count = {};
+    
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+    
+    for (let char of t) {
+        if (!count[char]) return false;
+        count[char]--;
+    }
+    
+    return true;
+};
+
+// Example usage:
+console.log(isAnagram("anagram", "nagaram")); // Output: true
+console.log(isAnagram("rat", "car"));         // Output: false
